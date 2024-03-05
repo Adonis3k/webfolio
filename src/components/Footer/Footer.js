0855d82150a9c9031e67e570d6391f1464eedd4a
Adonis3k <adonisdmadera@gmail.com>
2024-03-05 09:35:46 -0500

    more edits

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,10 +4,11 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className='Footer-container'>
-       DESIGNED AND BUILT BY:
         <a href="https://www.linkedin.com/in/adonis-madera/" target="_blank" rel="noreferrer">
-           ADONIS MADERA
-        </a>
+       DESIGNED AND BUILT BY:ADONIS MADERA
+        
+       </a>
+       ALL RIGHTS RESERVED © 2024 
       </div>
     
   );
