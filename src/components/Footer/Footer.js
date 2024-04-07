80adf5d6afb00257894f6e7b1368c9ccf04674a9
Adonis3k <adonisdmadera@gmail.com>
2024-04-07 18:29:16 -0400

    mobile friendly

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,8 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className='Footer-container'>
-        <a href="https://www.linkedin.com/in/adonis-madera/" target="_blank" rel="noreferrer">
-       Designed and built by: Adonis Madera
+       
+       <a href="https://github.com/bchiang7/v4" target="_blank" rel="noreferrer"> Designed Brittnay Chiang,</a> Built from Scratch by  <a href="https://www.linkedin.com/in/adonis-madera/" target="_blank" rel="noreferrer">Adonis Madera
         
        </a>
      
